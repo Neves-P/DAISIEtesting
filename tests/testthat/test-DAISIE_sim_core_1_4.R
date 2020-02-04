6b0dd14895e30d666fe0f10de0e886a6eacfa7f1
Neves-P <p.m.santos.neves@rug.nl>
2020-02-04 23:20:24 +0100

    Cover (hopefully) all of ONEcolonist_1_4

--- tests/testthat/test-DAISIE_sim_core_1_4.R
+++ tests/testthat/test-DAISIE_sim_core_1_4.R
@@ -210,4 +210,19 @@ test_that("DAISIE_sim_core_1_4 with n_immig > 0 and anagesis works", {
   )
 })
 
-
+test_that("DAISIE_sim_core_1_4 works with empty island", {
+  set.seed(42)
+  sim_time <- 10
+  n_mainland_species <- 10
+  clado_rate <- 0.00001
+  ext_rate <- 0.00001
+  carr_cap <- 4
+  imm_rate <- 0.00000000001
+  ana_rate <- 1.0
+  expect_silent(DAISIE_sim_core_1_4(
+    time = sim_time,
+    mainland_n = n_mainland_species,
+    pars = c(clado_rate, ext_rate, carr_cap, imm_rate, ana_rate)
+  )
+  )
+})
