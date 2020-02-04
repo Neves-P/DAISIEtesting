83d6fa591c4c901152f4771ec68174467e455943
Neves-P <p.m.santos.neves@rug.nl>
2020-02-04 23:04:25 +0100

    DAISIE_sim_core_checked is no more

--- R/DAISIE_sim_core_1_4.R
+++ R/DAISIE_sim_core_1_4.R
@@ -228,64 +228,14 @@ DAISIE_sim_core_1_4 <- function(time, mainland_n, pars) {
   return(island)
 }
 
-#' Runs one DAISIE simulation with a clade-specific carrying capacity.
-#' Version of \code{DAISIE_sim_core} that checks all its inputs
-#' and uses descriptively named arguments
-#' @param sim_time length of the simulated time
-#' @param n_mainland_species number of mainland species
-#' @param clado_rate cladogenesis rate
-#' @param ext_rate extinction rate
-#' @param carr_cap carrying capacity
-#' @param imm_rate immigration rate
-#' @param ana_rate anagenesis rate
-#' @return a list with these elements:
-#' \describe{
-#'   \item{stt_table}{a species-through-time table}
-#'   \item{branching_times}{branching times}
-#'   \item{stac}{
-#'     the status of the colonist
-#'     \itemize{
-#'       \item{1: \code{Non_endemic_MaxAge} (?immigrant is present but has not formed an extant clade)}
-#'       \item{2: \code{Endemic} (?immigrant is not present but has formed an extant clade)}
-#'       \item{3: \code{Endemic&Non_Endemic} (?immigrant is present and has formed an extant clade)}
-#'       \item{4: \code{Non_endemic} (?immigrant is present but has not formed an extant clade, and it is known when it immigrated)}
-#'     }
-#'   }
-#'   \item{missing_species}{number of missing species}
-#'   \item{other_clades_same_ancestor}{(not always present) ?no idea}
-#' }
-#' @author Richel J.C. Bilderbeek
-DAISIE_sim_core_checked_1_4 <- function(
-  sim_time,
-  n_mainland_species,
-  clado_rate,
-  ext_rate,
-  carr_cap,
-  imm_rate,
-  ana_rate
-) {
-  testit::assert(sim_time > 0.0)
-  testit::assert(n_mainland_species > 0)
-  testit::assert(clado_rate >= 0.0)
-  testit::assert(ext_rate >= 0.0)
-  testit::assert(carr_cap > 0)
-  testit::assert(imm_rate > 0.0)
-  testit::assert(ana_rate >= 0.0)
-  DAISIE_sim_core_1_4(
-    time = sim_time,
-    mainland_n = n_mainland_species,
-    pars = c(clado_rate, ext_rate, carr_cap, imm_rate, ana_rate)
-  )
-}
-
-
 #' Old version of DAISIE_ONEcolonist
 #'
-#' @param time stub
-#' @param island_spec stub
-#' @param stt_table stub
+#' @param time Total time of simulation
+#' @param island_spec Matrix with runtime simulation data
+#' @param stt_table Matrix with species-through-time information on immigrant,
+#' anagenetic, cladogenetic and total number of species.
 #'
-#' @return descendants
+#' @return DAISIE simulation output object
 #' @export
 DAISIE_ONEcolonist_1_4 <- function(time, island_spec, stt_table) {
   ### number of independent colonisations
