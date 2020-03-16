fbdb75d150543e03d2e0215ab4ec85fa25b64934
joshwlambert <j.w.l.lambert@rug.nl>
2020-03-16 13:32:28 +0000

    Renamed DAISIE_sim_core_1_4 to DAISIE_sim_core_1_4a and made bug fix

--- R/DAISIE_sim_core_1_4a.R
+++ R/DAISIE_sim_core_1_4a.R
@@ -129,7 +129,8 @@ DAISIE_sim_core_1_4 <- function(time, mainland_n, pars) {
             #carries a record of the most recent speciation
             if (mostrecentspl == "A") {
               #change the splitting date of the sister species so that it inherits the early splitting that used to belong to A.
-              tochange <- possiblesister[which(island_spec[possiblesister, 6] == max(as.numeric(island_spec[possiblesister, 6])))]
+              #bug fix here thanks to Nadiah Kristensen: max -> min
+              tochange <- possiblesister[which(island_spec[possiblesister, 6] == min(as.numeric(island_spec[possiblesister, 6])))]
               island_spec[tochange, 6] <- island_spec[extinct, 6]
             }
             #remove the offending A/B from these species
