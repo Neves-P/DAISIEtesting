251372394571673ae26d430e273f8a7b26cc1262
joshwlambert <j.w.l.lambert@rug.nl>
2020-04-13 20:18:15 +0100

    fixed get_ana_rate tests

--- tests/testthat/test-get_ana_rate.R
+++ tests/testthat/test-get_ana_rate.R
@@ -1,21 +1,10 @@
 context("get_ana_rate")
 
-test_that("use area constant diversity-independent without hyper_pars", {
+test_that("use area constant diversity-independent", {
   ps_ana_rate <- 1
   n_immigrants <- 5
-  default_pars <- DAISIE:::create_default_pars(
-    island_ontogeny = 0,
-    sea_level = 0,
-    totaltime = 1,
-    hyper_pars = NULL,
-    dist_pars = NULL,
-    ext_pars = NULL,
-    area_pars = NULL
-  )
   expect_silent(ana_rate <- DAISIE:::get_ana_rate(
     laa = 1,
-    hyper_pars = default_pars$hyper_pars,
-    dist_pars = default_pars$dist_pars,
     num_immigrants = 5)
   )
   expect_true(is.numeric(ana_rate))
@@ -25,22 +14,16 @@ test_that("use area constant diversity-independent without hyper_pars", {
   )
   created <- DAISIE:::get_ana_rate(
     laa = 1,
-    hyper_pars = default_pars$hyper_pars,
-    dist_pars = default_pars$dist_pars,
     num_immigrants = 5
   )
   expect_equal(expected, created)
 })
 
-test_that("use area constant diversity-independent with hyper_pars", {
+test_that("use area constant diversity-independent", {
   ps_ana_rate <- 1
   n_immigrants <- 5
-  hyper_pars <- DAISIE:::create_hyper_pars(d_0 = 1, x =  1, alpha =  1, beta =  1)
-  dist_pars <- DAISIE:::create_dist_pars(D = 1)
   expect_silent(ana_rate <- DAISIE:::get_ana_rate(
     laa = ps_ana_rate,
-    hyper_pars = hyper_pars,
-    dist_pars = dist_pars,
     num_immigrants = 5
   )
   )
@@ -51,8 +34,6 @@ test_that("use area constant diversity-independent with hyper_pars", {
   )
   created <- DAISIE:::get_ana_rate(
     laa = 1,
-    hyper_pars = hyper_pars,
-    dist_pars = dist_pars,
     num_immigrants = 5
   )
   expect_equal(expected, created)
