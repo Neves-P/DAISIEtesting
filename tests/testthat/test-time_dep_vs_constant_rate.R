08e0c5ee5087aae657f0a6b0f96072fef2f87277
Neves-P <p.m.santos.neves@rug.nl>
2020-04-23 15:37:24 +0200

    Correct hyper pars in timpe_dep vs constant_dep

--- tests/testthat/test-time_dep_vs_constant_rate.R
+++ tests/testthat/test-time_dep_vs_constant_rate.R
@@ -18,6 +18,17 @@ test_that("constant rate output matches time dependent code", {
   imm_rate <- 1.0
   ana_rate <- 1.0
   pars <- c(clado_rate, ext_rate, carr_cap, imm_rate, ana_rate)
+  area_pars <- DAISIE::create_area_pars(
+    max_area = 1,
+    current_area = 1,
+    proportional_peak_t = 0,
+    total_island_age = 10,
+    sea_level_amplitude = 0,
+    sea_level_frequency = 0,
+    island_gradient_angle = 0
+  )
+  hyper_pars <- DAISIE::create_hyper_pars(d = 0, x = 0)
+  nonoceanic_pars <- c(0, 0)
   rng_seed <- 42
   set.seed(rng_seed)
   constant_rate_out <- DAISIE:::DAISIE_sim_constant_rate(
@@ -32,38 +43,33 @@ test_that("constant rate output matches time dependent code", {
 
 
   #   Ontogeny code running constant case -----------------------------------
-  area_pars <- DAISIE::create_area_pars(
-    max_area = 1,
-    proportional_peak_t = 0,
-    peak_sharpness = 0,
-    total_island_age = sim_time,
-    sea_level_amplitude = 0,
-    sea_level_frequency = 0,
-    island_gradient_angle = 0
-  )
+  # We must use the core function to avoid calling calc_peak with a constant
+  # area.
+  hyper_pars <- DAISIE::create_hyper_pars(d = 0, x = 0)
   set.seed(rng_seed)
-  time_dependent_out <- DAISIE::DAISIE_sim_time_dependent(
+  time_dependent_out <- DAISIE:::DAISIE_sim_core_time_dependent(
     time = sim_time,
-    M = n_mainland_species,
+    mainland_n = n_mainland_species,
     pars = c(clado_rate, ext_rate, carr_cap, imm_rate, ana_rate),
-    replicates = 1,
     island_ontogeny = "beta",
     sea_level = "const",
     area_pars = area_pars,
-    ext_pars = c(ext_rate, 0),
-    plot_sims = FALSE,
-    verbose = FALSE,
-    sample_freq = Inf
+    hyper_pars = hyper_pars,
+    nonoceanic_pars = nonoceanic_pars,
+    Amax = 1,
+    Amin = 1,
+    peak = 1
   )
 
   expect_equal(
-    time_dependent_out[[1]][[1]]$stt_all[2, ],
-    constant_rate_out[[1]][[1]]$stt_all[2, ]
+    time_dependent_out[[1]][2, ],
+    constant_rate_out[[1]][[1]]$stt_all[2, 1:4]
   )
 
   # Following lines will necessarily be different, see note.
   expect_true(
-    !all(time_dependent_out[[1]][[1]]$stt_all[3, ] !=
-      constant_rate_out[[1]][[1]]$stt_all[3, ])
-    )
+    !all(time_dependent_out[[1]][3, ] ==
+         constant_rate_out[[1]][[1]]$stt_all[3, 1:4])
+  )
 })
+
